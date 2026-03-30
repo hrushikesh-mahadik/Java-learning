3513de79c5559a04ff1e86558413a744cdba3049
Hrushikesh Mahadik <147132574+themahadik@users.noreply.github.com>
2026-03-30 18:22:59 +0530

    Core-Java

---
+++ Core-Java/04_anonymous/anonymous.java
@@ -0,0 +1,14 @@
+public class anonymous {
+    
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+        @Override
+        void speak(){
+            System.out.println("woof");
+        }
+      dog1.speak();
+      dog2.speak();
+
+    }
+}
