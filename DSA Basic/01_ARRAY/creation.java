dd40aa012dd2ed94cd06beca844fb9cf2ff02d8c
Hrushikesh Mahadik <147132574+themahadik@users.noreply.github.com>
2026-04-01 23:26:46 +0530

    DSA BASIC

---
+++ DSA Basic/01_ARRAY/creation.java
@@ -0,0 +1,12 @@
+public class creation{
+    
+    public static void main(String[] args) {
+        // creation of array
+        int marks[] = new int[50];
+        int numbers[] = {1,2,3};
+        int moreNo  [] = {4,5,6};
+        String fruits[] = {"apple","bannana"};
+        System.out.print(numbers[2]);
+    }  
+    
+}
